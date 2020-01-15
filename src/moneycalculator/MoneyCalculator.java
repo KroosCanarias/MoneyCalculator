2b589ab821f2c34c48a5cb5290b2e853283e1c7a
thanq <thanq@192.168.56.1>
2020-01-15 16:22:07 +0000

    Version 2 Modelo CIPO

--- src/moneycalculator/MoneyCalculator.java
+++ src/moneycalculator/MoneyCalculator.java
@@ -1,21 +1,21 @@
 package moneycalculator;
 
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
 import java.util.Scanner;
 
 public class MoneyCalculator { 
-    public static void main(String[] args) throws Exception {
-        System.out.println("Introduce una cantidad de dólares: ");
-        Scanner scanner = new Scanner(System.in);
-        double amount = scanner.nextDouble();
-        double exchangerate = getExchangeRate("USD","EUR");
-        System.out.println(amount + " dolares equivalen a " + amount*exchangerate + " euros");        
+    double moneyFrom;
+    double to;
+    public static void main(String[] args) throws IOException {
+        MoneyCalculator moneycalculator = new MoneyCalculator();
+        moneycalculator.control();
     }
-    
-    private static double getExchangeRate(String from, String to) throws Exception {
+
+    private static double getExchangeRate(String from, String to) throws IOException {
         URL url = 
             new URL("http://free.currencyconverterapi.com/api/v5/convert?q=" +
                     from + "_" + to + "&compact=y");
@@ -27,5 +27,23 @@ public class MoneyCalculator {
             String line1 = line.substring(line.indexOf(to)+12, line.indexOf("}"));
             return Double.parseDouble(line1);
         }
+    }
+
+    private void control() throws IOException {
+        input();
+        process();
+        output();
+    }
+
+    private void input() {
+        System.out.println("Introduce una cantidad de dólares: ");
+        Scanner scanner = new Scanner(System.in);
+        moneyFrom = scanner.nextDouble();
+    }
+    private void process() throws IOException {
+        to = getExchangeRate("USD","EUR");
+    }
+    private void output() {
+        System.out.println(moneyFrom + " $ = " + moneyFrom*to + " €");
     }    
 }
